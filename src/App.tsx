04c67190bbb6d0568f724880ffe3060f80a95e4c
Alison Valderrama <alisvalderrama@gmail.com>
2025-03-22 21:50:43 -0500

    update login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
-import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </Router>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/login" element={<LoginPage />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
